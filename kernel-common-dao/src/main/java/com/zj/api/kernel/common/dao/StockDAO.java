af9d7cdca380e3099f1969dcd302f7bf82b86c88
zj974185723 <974185723@qq.com>
2017-01-16 14:51:09 +0800

    基础类

---
+++ kernel-common-dao/src/main/java/com/zj/api/kernel/common/dao/StockDAO.java
@@ -0,0 +1,9 @@
+package com.zj.api.kernel.common.dao;
+
+import com.zj.api.model.Stock;
+
+/**
+ * Created by js on 2017/1/16.
+ */
+public interface StockDAO extends BaseDAO<Stock> {
+}
---
+++ kernel-common-dao/src/main/java/com/zj/api/kernel/common/dao/impl/StockDAOImpl.java
@@ -0,0 +1,13 @@
+package com.zj.api.kernel.common.dao.impl;
+
+import com.zj.api.kernel.common.dao.StockDAO;
+import com.zj.api.model.Stock;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by js on 2017/1/16.
+ */
+@Repository("stockDAO")
+public class StockDAOImpl extends BaseDAOImpl<Stock> implements StockDAO {
+
+}
